fa5d33ca752ff31b19bb28db6af94130e31cb0f9
Frank Schmitt <frankschmitt@gmx.de>
2022-06-25 10:53:07 +0200

    include git commit hash in version info (WIP)

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
-#[cfg(windows)]
+#[cfg(target_os="windows")]
 extern crate winres;
+#[cfg(target_os="windows")]
+extern crate vergen;
 
-#[cfg(windows)]
+#[cfg(target_os="windows")]
 fn main() {
+    use vergen::{Config, vergen};
+    // Generate the default 'cargo:' instruction output
+    //let _vers = vergen(Config::default());
     let res = winres::WindowsResource::new();
     //res.set_icon("xanthidae.ico");
     res.compile().unwrap();
 }
-
-#[cfg(unix)]
+#[cfg(target_os="linux")]
 fn main() {}
--- src/prelude.rs
+++ src/prelude.rs
@@ -59,6 +59,8 @@ const POPUP_ITEM_NAME_REPEATABLE_AND_VERSIONED_MIGRATION: &str =
 
 const VERSION_INFO_CAPTION: &[u8] = b"Version info\0";
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+//const BUILD_TIMESTAMP: &str = env!("VERGEN_BUILD_TIMESTAMP");
+//const GIT_SEMVER: &str = env!("VERGEN_GIT_SEMVER");
 
 static mut PLUGIN_ID: c_int = 0;
 
@@ -69,7 +71,9 @@ lazy_static! {
     pub static ref API: RwLock<Box<dyn PlsqlDevApi + Send + Sync>> = RwLock::new(Box::new(NativePlsqlDevApi::new()));
     pub static ref CONFIG: RwLock<Config> = RwLock::new(Config::default());
     static ref VERSION_MESSAGE: CString = CString::new(format!(
-        "This is version {} of Xanthidae, a plugin written in Rust",
+        //"This is version {}-{} of Xanthidae, a plugin written in Rust, build date: {}",
+        //VERSION, GIT_SEMVER, BUILD_TIMESTAMP
+        "This is version {}of Xanthidae, a plugin written in Rust",
         VERSION
     ))
     .unwrap();
